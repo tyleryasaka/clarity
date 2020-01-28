b22890cf9b08615a3b5ac9749bea7b9bd4591f37
Tyler Yasaka <contact@tyleryasaka.me>
2020-01-27 21:40:01 -0800

    Move to separate file

---
+++ tactile-js/src/definitions.js
@@ -0,0 +1,163 @@
+const variableTypes = [
+  'function',
+  'domain',
+  'value'
+]
+
+const primitiveValidators = {
+  'string-literal': new RegExp('.*'),
+  'bool-literal': new RegExp('^(true|false)$'),
+  'integer-literal': new RegExp('^\\d+$'),
+  'domain-literal': new RegExp('^(string|integer|bool|function)$')
+}
+
+const multiTypes = {
+  value: ['application', 'ifelse', 'function', 'integer-literal', 'string-literal', 'bool-literal'],
+  domain: ['domain-literal', 'function-signature']
+}
+
+const objValidators = {
+  program: [
+    {
+      key: 'functions',
+      type: 'function-definition',
+      list: true
+    }
+  ],
+  'function-definition': [
+    {
+      key: 'id',
+      type: 'string-literal',
+      list: false
+    },
+    {
+      key: 'function',
+      type: 'function',
+      list: false
+    }
+  ],
+  function: [
+    {
+      key: 'name',
+      type: 'string-literal',
+      list: false
+    },
+    {
+      key: 'description',
+      type: 'string-literal',
+      list: false
+    },
+    {
+      key: 'domainParams',
+      type: 'domainParam',
+      list: true
+    },
+    {
+      key: 'valueParams',
+      type: 'valueParam',
+      list: true
+    },
+    {
+      key: 'domain',
+      type: 'domain',
+      list: false
+    },
+    {
+      key: 'body',
+      type: 'value',
+      list: false
+    }
+  ],
+  domainParam: [
+    {
+      key: 'name',
+      type: 'string-literal',
+      list: false
+    },
+    {
+      key: 'description',
+      type: 'string-literal',
+      list: false
+    }
+  ],
+  valueParam: [
+    {
+      key: 'name',
+      type: 'string-literal',
+      list: false
+    },
+    {
+      key: 'description',
+      type: 'string-literal',
+      list: false
+    },
+    {
+      key: 'domain',
+      type: 'domain',
+      list: false
+    }
+  ],
+  'function-signature': [
+    {
+      key: 'domain',
+      type: 'domain',
+      list: false
+    },
+    {
+      key: 'domainParamsCount',
+      type: 'integer-literal',
+      list: false
+    },
+    {
+      key: 'valueParamDomains',
+      type: 'domain',
+      list: true
+    }
+  ],
+  application: [
+    {
+      key: 'function',
+      type: 'string-literal',
+      list: false
+    },
+    {
+      key: 'valueArgs',
+      type: 'value',
+      list: true
+    },
+    {
+      key: 'domainArgs',
+      type: 'domain',
+      list: true
+    }
+  ],
+  ifelse: [
+    {
+      key: 'domain',
+      type: 'domain',
+      list: false
+    },
+    {
+      key: 'condition',
+      type: 'value',
+      list: false
+    },
+    {
+      key: 'if',
+      type: 'value',
+      list: false
+    },
+    {
+      key: 'else',
+      type: 'value',
+      list: false
+    }
+  ]
+}
+
+module.exports = {
+  variableTypes,
+  primitiveValidators,
+  multiTypes,
+  objValidators
+}

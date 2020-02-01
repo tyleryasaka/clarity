c0defe79888ffe0beb5fef283646b3f10de085f4
Tyler Yasaka <contact@tyleryasaka.me>
2020-01-31 19:46:38 -0800

    Add more syntax validation

--- tactile-js/test/validate-syntax-test.js
+++ tactile-js/test/validate-syntax-test.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const assert = require('assert')
 
 const programJSON = fs.readFileSync('./test/stubs/program.json', 'utf8')
+const invalid1 = fs.readFileSync('./test/stubs/invalid-syntax/invalid1.json', 'utf8')
+const invalid2 = fs.readFileSync('./test/stubs/invalid-syntax/invalid2.json', 'utf8')
 const validateSyntax = require('../src/validate-syntax')
 
 describe('validateSyntax', function () {
@@ -11,5 +13,12 @@ describe('validateSyntax', function () {
     const result = validateSyntax(JSON.parse(programJSON))
     assert.strictEqual(result.errorCode, '')
     assert.ok(result.isValid)
+  })
+
+  it('should not validate nonsense programs', function () {
+    const result = validateSyntax(JSON.parse(invalid1))
+    assert.strictEqual(result.isValid, false)
+    const result2 = validateSyntax(JSON.parse(invalid2))
+    assert.strictEqual(result2.isValid, false)
   })
 })
